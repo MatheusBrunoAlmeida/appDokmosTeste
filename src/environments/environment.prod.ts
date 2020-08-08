a4759f9b848c0eac4e9a32d615797c643cfe8182
MatheusBrunoAlmeida <50282082+MatheusBrunoAlmeida@users.noreply.github.com>
2020-08-08 13:35:13 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDXv3Eh-00PzP1SqYsYRW2aNls0QkTazS4",
-    authDomain: "app-campea.firebaseapp.com",
-    databaseURL: "https://app-campea.firebaseio.com",
-    projectId: "app-campea",
-    storageBucket: "app-campea.appspot.com",
-    messagingSenderId: "1094972190561",
-    appId: "1:1094972190561:web:0efe64a34673152a791f20",
-    measurementId: "G-NFBR0RL7E9"
-  }
+  firebase: {'keys}
 };
